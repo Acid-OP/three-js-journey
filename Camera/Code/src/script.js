59073936034c77b9194e5e018891be2c486de4fa
Gaurav Kapur <gauravkapur596@gmail.com>
2024-12-29 05:13:27 +0530

    Added Geometrics , Materials , Resizing , Textures in threejs

--- Camera/Code/src/script.js
+++ Camera/Code/src/script.js
@@ -1,12 +1,24 @@
 import './style.css'
 import * as THREE from 'three'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
 /**
  * Base
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+// Cursor 
+const cursor = {
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove',(event)=>{
 
+    cursor.x = event.clientX/sizes.width - 0.5
+    cursor.y = -(event.clientY/sizes.height - 0.5)
+
+
+})
 // Sizes
 const sizes = {
     width: 800,
@@ -28,9 +40,12 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.x = 2
 camera.position.y = 2
 camera.position.z = 2
-camera.lookAt(mesh.position)
+
 scene.add(camera)
 
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
@@ -45,7 +60,13 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // camera.position.x = Math.sin(cursor.x*Math.PI*2)*3
+    // camera.position.z = Math.cos(cursor.x*Math.PI*2)*3
+
+    // camera.position.y = cursor.y*5
+    // camera.lookAt(new THREE.Vector3())
+
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
